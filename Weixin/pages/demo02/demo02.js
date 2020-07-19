b6b7337ac9d695a8a0e4ba4a805ca23dceefffec
chenjia-151 <1020420948@qq.com>
2020-07-19 17:26:23 +0800

    学习

---
+++ Weixin/pages/demo02/demo02.js
@@ -0,0 +1,34 @@
+// pages/demo02/demo02.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg:"hello mina!",
+    num:10000,
+    isGirl:false,
+    person:{
+      age:45,
+      height:150,
+      weight:200,
+      name:"富婆"
+    },
+    isChecked:false,
+    list:[
+      {
+        id:0,
+        name:'猪八戒'
+      },
+      {
+        id:1,
+        name:'天蓬元帅'
+      },
+      {
+        id:2,
+        name:'悟能'
+      }
+    ]
+    }
+ 
+})

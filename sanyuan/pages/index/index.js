b2e164a167422bef4755bbe7c9a24e48596e2465
chenjia-151 <1020420948@qq.com>
2020-07-29 16:32:28 +0800

    小程序学习

---
+++ sanyuan/pages/index/index.js
@@ -0,0 +1,31 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    bannerList:[],
+    recommendList:[],
+    isRecommendListLoading:true
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    wx.request({
+      url: 'url',
+      seccess()
+      })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})

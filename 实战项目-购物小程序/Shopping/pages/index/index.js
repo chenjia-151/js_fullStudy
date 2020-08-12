b4458778e4320e83ddca797689b944ae0c88a9be
chenjia-151 <1020420948@qq.com>
2020-08-12 12:59:55 +0800

    实现首页

--- 实战项目-购物小程序/Shopping/pages/index/index.js
+++ 实战项目-购物小程序/Shopping/pages/index/index.js
@@ -1,39 +1,62 @@
-//Page Object
+// 引入 用来发送请求 的方法  一定要把路径补全
+import { request } from "../../request/index.js"
 Page({
   data: {
-    
+    // 轮播图数组
+    swiperList: [],
+
+    // 导航 数组
+    catesList: [],
+
+    // 楼层数据
+    floorList: [],
   },
-  //options(Object)
-  onLoad: function(options) {
-    
-  },
-  onReady: function() {
-    
-  },
-  onShow: function() {
-    
-  },
-  onHide: function() {
+  // 页面开始加载 就会触发
+  onLoad: function (options) {
+    // // 发送异步请求，获取轮播图数据 
+    // // 优化的手段可以通过es6的 promise 来解决 
+    // var reqTask = wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList:result.data.message
+    //     })
+    //   }
+    // });
+
+    this.getSwiperList();
+    this.getCatesList();
+    this.getFloorList();
 
   },
-  onUnload: function() {
 
+  // 获取轮播图数据
+  getSwiperList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata" })
+      .then(result => {
+        this.setData({
+          swiperList: result.data.message
+        })
+      })
   },
-  onPullDownRefresh: function() {
 
+  // 获取轮播图数据
+  getCatesList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems" })
+      .then(result => {
+        this.setData({
+          catesList: result.data.message
+        })
+      })
   },
-  onReachBottom: function() {
 
-  },
-  onShareAppMessage: function() {
-
-  },
-  onPageScroll: function() {
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item) {
-
+  // 获取楼层数据
+  getFloorList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/floordata" })
+      .then(result => {
+        this.setData({
+          floorList: result.data.message
+        })
+      })
   }
 });
-  

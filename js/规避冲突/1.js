da36d4da1839b137ef661760bb2ca0f0cbdcf63e
chenjia-151 <1020420948@qq.com>
2020-08-15 14:33:43 +0800

    规避冲突

---
+++ js/规避冲突/1.js
@@ -0,0 +1,11 @@
+function foo() {
+    function bar(a) {
+        i = 3;
+        // console.log(a, '----------------');
+        console.log(a + i);
+    }
+    for (var i = 0; i < 10; i++) {
+        bar(i * 2);
+    }
+}
+foo()

51b3a5a88f5bde43476a0af2b70a63433b649b96
chenjia-151 <1020420948@qq.com>
2020-08-25 12:04:48 +0800

    Vue-路由传参

---
+++ Vue/vue-routers/src/components/Hi.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+      <h1>{{msg}}</h1>
+      <router-link :to="{name: 'hi1',data:{name: '张三'}}">h1 一</router-link>
+      <span style="display:inline-block;width: 100px" ></span>
+      <router-link to="/hi/hi2">h1 二</router-link>
+
+      <router-view></router-view>
+  </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Hi',
+
+    // 统一将data数据写成一个函数l，返回一个对象
+    data(){
+        return {
+            msg: 'Hi, I am wn'
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ Vue/vue-routers/src/components/Hi1.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hi">
+      我是hi1页面
+      <!-- {{$route.name}} -->
+      {{$route.data.name}}
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

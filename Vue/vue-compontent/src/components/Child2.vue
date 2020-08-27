27fd45e84622b7f680fff88c4f31f6f3ec739187
chenjia-151 <1020420948@qq.com>
2020-08-27 15:13:21 +0800

    父子组件之间的通信

---
+++ Vue/vue-compontent/src/components/Child2.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="clild2">
+      child2 {{msg}}
+  </div>
+</template>
+
+<script>
+import bus from '@/utils/bus.js'
+export default {
+    data(){
+        return {
+            msg: ''
+        }
+    },
+    created(){
+        bus.$on('info', (val)=>{      //  $on  接收数据   与  $emit发散数据   一起使用
+            console.log(val);
+            this.msg = val
+        })          
+    }
+}
+</script>
+
+<style>
+
+</style>

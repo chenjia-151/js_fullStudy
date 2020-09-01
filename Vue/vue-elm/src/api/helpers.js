6390ecd97385ffb66822ff26ba11ba8aa26247e4
chenjia-151 <1020420948@qq.com>
2020-09-01 17:01:39 +0800

    未完成

--- Vue/vue-elm/src/api/helpers.js
+++ Vue/vue-elm/src/api/helpers.js
@@ -1,9 +1,21 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const baseUrl = 'http://ustbhuangyi.com/sell/'
+const baseUrl = 'http://ustbhuangyi.com/sell/';
 
 export function get(url) {
-    return function(params = {}){
-        return axios.get(baseUrl + url)
-    }
+  return function (params = {}) {
+    return axios.get(baseUrl + url, {
+      params,
+    }).then((res) => {
+      const { errno, data } = res.data;
+      // 对象的解构  就相当于下面这两个
+      // const errno = res.data.errno
+      // const data = res.data.data
+      if (errno === 0) {
+        return data;
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
 }

261323c1624893be2b4b4abced2f33b980686651
chenjia-151 <1020420948@qq.com>
2020-09-01 22:58:41 +0800

    商品详情页部分功能完成

--- 黑马项目/实战项目-购物小程序/Shopping/pages/goods_detail/goods_detail.js
+++ 黑马项目/实战项目-购物小程序/Shopping/pages/goods_detail/goods_detail.js
@@ -1,66 +1,37 @@
-// pages/goods_detail/goods_detail.js
+import { request } from "../../request/index.js"
+// import regeneratorRuntime from '../../lib/runtime/runtime'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const { goods_id } = options
+    // console.log(goods_id);
+    this.getGoodsDatial(goods_id)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取商品的详情数据
+  async getGoodsDatial(goods_id) {
+    const goodsObj = await request({ url: "/goods/detail", data: { goods_id } })
+    // console.log(res);
+    this.setData({
+      goodsObj: {
+        goods_name: goodsObj.data.message.goods_name,
+        goods_price: goodsObj.data.message.goods_price,
+        // iphone部分手机 不识别 webp图片格式
+        // 确保将图片修改 1.webp => 1.jpg 格式
+        goods_introduce: goodsObj.data.message.goods_introduce.replace(/\.webp/g,'.jpg'),
+        pics: goodsObj.data.message.pics,
+      }
+    })
   }
 })

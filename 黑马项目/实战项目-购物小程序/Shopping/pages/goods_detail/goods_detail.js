2e7596d5a05f730a7317520feda57f3a891322f3
chenjia-151 <1020420948@qq.com>
2020-09-02 18:07:23 +0800

    购物车页面ing

--- 黑马项目/实战项目-购物小程序/Shopping/pages/goods_detail/goods_detail.js
+++ 黑马项目/实战项目-购物小程序/Shopping/pages/goods_detail/goods_detail.js
@@ -10,6 +10,9 @@ Page({
     goodsObj: {}
   },
 
+  // 商品对象
+  GoodsInfo: {},
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -22,6 +25,7 @@ Page({
   // 获取商品的详情数据
   async getGoodsDatial(goods_id) {
     const goodsObj = await request({ url: "/goods/detail", data: { goods_id } })
+    this.GoodsInfo = goodsObj;
     // console.log(res);
     this.setData({
       goodsObj: {
@@ -29,9 +33,43 @@ Page({
         goods_price: goodsObj.data.message.goods_price,
         // iphone部分手机 不识别 webp图片格式
         // 确保将图片修改 1.webp => 1.jpg 格式
-        goods_introduce: goodsObj.data.message.goods_introduce.replace(/\.webp/g,'.jpg'),
+        goods_introduce: goodsObj.data.message.goods_introduce.replace(/\.webp/g, '.jpg'),
         pics: goodsObj.data.message.pics,
       }
+    })
+  },
+
+  // 点击轮播图放大预览
+  handlePrevewImage(e) {
+    // console.log("预览");
+    const urls = this.GoodsInfo.data.message.pics.map(v => v.pics_mid)
+    const current = e.currentTarget.dataset.url
+    wx.previewImage({
+      current,
+      urls
+    })
+  },
+
+  // 点击加入购物车
+  handleCartAdd() {
+    let cart = wx.getStorageSync('cart') || [];
+    let index = cart.findIndex(v => v.goods_id === this.GoodsInfo.goods_id);
+    if(index===-1){
+      // 不存在
+      this.GoodsInfo.num = 1
+      cart.push(this.GoodsInfo)
+    }else{
+      // 存在  执行 num++
+      cart[index].num++
+    }
+
+    // 把购物车重新添加进缓存中
+    wx.setStorageSync('cart', cart)
+    // 弹窗提示
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      mask:true
     })
   }
 })

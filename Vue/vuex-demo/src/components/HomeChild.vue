33f0c2c4476924df37f01cf77cfb6433217beee6
chenjia-151 <1020420948@qq.com>
2020-09-07 20:10:28 +0800

    Vuex-demo

--- Vue/vuex-demo/src/components/HomeChild.vue
+++ Vue/vuex-demo/src/components/HomeChild.vue
@@ -3,8 +3,9 @@
     <p>首页的子组件</p>
     <hr />
     <h3>{{count}}{{abc}}</h3>
+    <p>{{$store.state.a.count}}</p>
     <div class="btn">
-      <button @click="add">+</button> |
+      <button @click="addAction">+</button> |
       <button @click="reduce">-</button>
     </div>
   </div>
@@ -12,7 +13,7 @@
 
 <script>
 import store from "@/vuex/store.js";
-import { mapState, mapMutations, mapGetters } from "vuex";
+import { mapState, mapMutations, mapGetters, mapActions } from "vuex";
 export default {
   data() {
     return {
@@ -31,8 +32,8 @@ export default {
   // }),
 
   computed: {
-      ...mapState(["count"]),
-      ...mapGetters(['abc'])
+    ...mapState(["count"]),
+    ...mapGetters(["abc"]),
   },
 
   // methods:{
@@ -50,6 +51,7 @@ export default {
 
   methods: {
     ...mapMutations(["add", "reduce"]), // es6中的解构 相当于将 store.js 中的 add() reduce() 方法复制过来
+    ...mapActions(["addAction"]),
   },
 };
 </script>
--- Vue/vuex-demo/src/vuex/store.js
+++ Vue/vuex-demo/src/vuex/store.js
@@ -7,16 +7,16 @@ const state = {
     count: 1
 }
 
-// vuex 中的计算属性 getters  能够修改掉数据源中的属性值
+// vuex 中的计算属性 getters  能够修改掉数据源中的数据
 const getters = {
-    abc: () =>{
+    abc: () => {
         return state.count += 100
     }
 }
 
-const mutations = {
+const mutations = {    //  不建议使用 异步方法 会消耗性能
     add(state) {
-        state.count ++
+        state.count++
     },
     // 括号中的参数可以随便定义 但是定义的参数 就是 state中的内容
     reduce(state) {
@@ -24,8 +24,35 @@ const mutations = {
     }
 }
 
-export default new Vuex.Store({
+// 提交 mutations 里面的方法  如果有接口请求的话 建议 在 action 中使用异步方法 不建议在 mutation中使用异步
+const actions = {
+    addAction(context) {
+        context.commit('add')
+    }
+}
+
+const moduleA = {
     state,
     mutations,
-    getters
+    getters,
+    actions
+}
+
+const moduleB = {
+    state:{
+
+    },
+    mutations:{
+
+    },
+    getters:{
+
+    }
+}
+
+export default new Vuex.Store({
+    modules:{
+        a: moduleA,
+        b: moduleB
+    }
 })

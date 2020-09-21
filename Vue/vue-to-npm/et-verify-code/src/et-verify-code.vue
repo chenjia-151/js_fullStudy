74d47dbe87e43b8c3454eea24cfeafd2d1b2caf5
chenjia-151 <1020420948@qq.com>
2020-09-21 22:58:22 +0800

    npm-自定义组件

--- Vue/vue-to-npm/et-verify-code/src/et-verify-code.vue
+++ Vue/vue-to-npm/et-verify-code/src/et-verify-code.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="vcWrap">
-    <input
-      v-for="n in len"
-      :key="'codeInput' + n"
-      :ref="'codeInput' + n"
-      v-model.trim.number="code[n-1]"
-      @keydown="onkeydown(n)"
-      @keyup="onkeyup(n)"
-    />
+    <input v-for="n in len" :key="'codeInput' + n" :ref="'codeInput' + n" v-model.trim.number="code[n-1]" @keydown="onkeydown(n)" @keyup="onkeyup(n)">
   </div>
 </template>
 
 <script>
 export default {
-  name: "EtVerifyCode",
-  props: ["length"], // 子组价不允许修改父组件传过来的值，会报错
+  name: 'EtVerifyCode',
+  props: ['length'],
   data() {
     return {
       len: this.length || 4,
@@ -22,33 +15,31 @@ export default {
       keyCodes: [
         48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 8, 13
       ]
-    };
+    }
   },
   methods: {
-      onkeydown(n){
-        //   console.log(n);
-        if(!this.keyCodes.includes(event.keyCode)){  // 拦截keyCodes以外的数字
-            event.returnValue = false
-        } else if ( event.keyCode === 8 && n > 1 && this.code[n-1] === '') {
-            this.$refs['codeInput'+(n-1)][0].focus()
-            event.returnValue = false
-        }
-      },
-      onkeyup(n){
-        //   console.log(n);
-        if((event.keyCode >= 48 && event.keyCode <= 57) || event.keyCode >= 96 && event.keyCode <= 105){
-            this.$set(this.code, n - 1, event.key)
-            if(n < this.len) this.$refs['codeInput'+(n + 1)][0].focus()
-            // 往外抛出一个发布事件
-            this.$emit('sendCodeInput', this.code.join(''))
-        } else if (event.keyCode === 8) {
-            this.$emit('sendCodeInput', this.code.join(''))
-        } else if (event.keyCode === 13) {
-            if(this.code.join('').length === this.len) this.$emit('goNext')
-        }
+    onkeydown(n) {
+      if (!this.keyCodes.includes(event.keyCode)) {
+        event.returnValue = false
+      } else if (event.keyCode === 8 && n > 1 && this.code[n-1] === '') {
+        this.$refs['codeInput'+(n-1)][0].focus()
+        event.returnValue = false
       }
-  },
-};
+    },
+    onkeyup(n) {
+      if ((event.keyCode >= 48 && event.keyCode <=57) || (event.keyCode >= 96 && event.keyCode <= 105)) {
+        this.$set(this.code, n - 1, event.key)
+        if (n < this.len) this.$refs['codeInput'+(n + 1)][0].focus() 
+
+        this.$emit('sendCodeInput', this.code.join(''))
+      } else if (event.keyCode === 8) {
+        this.$emit('sendCodeInput', this.code.join(''))
+      } else if (event.keyCode === 13) {
+        if (this.code.join('').length === this.len) this.$emit('goNext')
+      }
+    }
+  }
+}
 </script>
 
 <style lang="stylus">

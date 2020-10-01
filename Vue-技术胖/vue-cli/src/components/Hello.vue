ebedc0fb1f4fe3a38d26f90f6e82c852d2c8f2ae
chenjia-151 <1020420948@qq.com>
2020-10-01 18:13:02 +0800

    vue-cli

---
+++ Vue-技术胖/vue-cli/src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+      <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            message: 'Hello World'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

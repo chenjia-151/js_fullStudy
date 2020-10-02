3f9ad77e4493014a533f86f9278d8e2b2e243c9a
chenjia-151 <1020420948@qq.com>
2020-10-02 14:48:55 +0800

    vue-cli

---
+++ Vue-技术胖/vue-cli/src/components/Love.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <input class="input" type="text" v-model="msg" /><br>
+    <button @click="add()">+</button> |
+    <button @click="desc()">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 1,
+    };
+  },
+  methods: {
+    add() {
+      this.msg++;
+    },
+    desc() {
+      this.msg--;
+    },
+  },
+};
+</script>
+
+<style>
+input{
+    margin-bottom: 20px;
+}
+</style>
---
+++ Vue-技术胖/vue-cli/src/views/Like.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      你好，中国
+      <love></love>
+  </div>
+</template>
+
+<script>
+import Love from '@/components/Love.vue'
+export default {
+  name: 'Like',
+  components:{
+    Love
+  }
+}
+</script>
+
+<style>
+
+</style>

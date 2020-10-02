5a29deae8b497557d4d306a33b35dab2d09637d3
chenjia-151 <1020420948@qq.com>
2020-10-02 14:30:54 +0800

    vue-cli

---
+++ Vue-技术胖/vue-cli/src/components/Compute.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+      <p>{{num}}</p>
+      <button @click="add">+</button> | 
+      <button @click="reduce">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            num: 0
+        }
+    },
+    methods:{
+        add(){
+            this.num++
+        },
+        reduce(){
+            this.num--
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

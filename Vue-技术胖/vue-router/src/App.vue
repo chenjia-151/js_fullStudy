77abd5735ae0c8b593ff1edf03fcc66317a8549c
chenjia-151 <1020420948@qq.com>
2020-10-10 16:32:07 +0800

    vue-router/编程式导航

--- Vue-技术胖/vue-router/src/App.vue
+++ Vue-技术胖/vue-router/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png"/><br>
+    <img src="./assets/logo.png" /><br />
+    <div id="id">
+      <button @click="goBack">后退</button>
+      <button @click="goGo">前进</button>
+      <button @click="goHome">返回首页</button>
+    </div>
     <router-link to="/">Home</router-link> |
-    <router-link to="/params/198/jack website is very good ">params</router-link> |
+    <router-link to="/params/198/jack website is very good ">params</router-link> | 
     <router-link to="/goHome">goHome</router-link> |
     <router-link to="/goParams/918/I like vue.js">goParams</router-link> |
     <router-link to="/hi1">Hi1</router-link> |
@@ -12,13 +17,23 @@
     <transition name="fade" mode="out-in">
       <router-view></router-view>
     </transition>
-    
   </div>
 </template>
 
 <script>
 export default {
   name: "app",
+  methods: {
+    goBack() {
+      this.$router.go(-1);  // 后退一个页面
+    },
+    goGo(){
+      this.$router.go(1)  // 前进一个页面
+    },
+    goHome(){
+      this.$router.push('/') // 可以直接选择跳转到某个页面
+    }
+  },
 };
 </script>
 
@@ -31,17 +46,17 @@ export default {
   color: #2c3e50;
   margin-right: 60px;
 }
-.fade-enter{
+.fade-enter {
   opacity: 0;
 }
-.fade-enter-active{
-  transition: opacity .5s;
+.fade-enter-active {
+  transition: opacity 0.5s;
 }
-.fade-leave{
+.fade-leave {
   opacity: 1;
 }
-.fade-leave-active{
+.fade-leave-active {
   opacity: 0;
-  transition: opacity .5s;
+  transition: opacity 0.5s;
 }
 </style>

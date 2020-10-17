bd0776adfc00af11596980dbc0325f0929265493
chenjia-151 <1020420948@qq.com>
2020-10-17 16:31:57 +0800

    uni-app

---
+++ uni-app/helloWorld/pages/home/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <view>测试页面</view>
+</template>
+
+<script>
+export default {
+  onload(option) {
+    console.log(option);
+  },
+  created() {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>

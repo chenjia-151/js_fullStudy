0e95b4590200cf12ce64c54ea44b242f3c27eead
chenjia-151 <1020420948@qq.com>
2020-10-27 23:14:10 +0800

    ...

--- js/柯里化/1.js
+++ js/柯里化/1.js
@@ -1,21 +1,5 @@
-const arr = []
-let a = [
-    { time: 1 },
-    { time: 5 },
-    { time: 8 },
-    { time: 11 },
-    { time: 16 },
-    { time: 17 },
-    { time: 29 },
-    { time: 34 },
-    { time: 39 }
-]
-let time_line = [3, 7, 12, 17, 19, 31, 40]
-for (let i = 0; i < time_line.length; i++) {
-    for (let j = 0; j < a.length; j++) {
-        if (time_line[i] > a[j].time && time_line[i] < a[j + 1].time) {
-            return a[j + 1]
-        }
-    }
-    return a[j+1]
-}
+let arr = [1,32,54,6,543];
+let res = arr.map(function(item,index,arr){
+	return item*2;
+})
+console.log(res)

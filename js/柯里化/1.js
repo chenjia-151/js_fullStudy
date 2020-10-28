86b76e77e721000b52b7a1187d805dbf1287200c
chenjia-151 <1020420948@qq.com>
2020-10-28 12:55:42 +0800

    vue组件传值

--- js/柯里化/1.js
+++
@@ -1,5 +0,0 @@
-let arr = [1,32,54,6,543];
-let res = arr.map(function(item,index,arr){
-	return item*2;
-})
-console.log(res)
---
+++ js/柯里化/2.vue
@@ -0,0 +1,48 @@
+// 父组件
+<template>
+  <div id="app">
+    <v-deal @transfer="getUser"></v-deal>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: "",
+    };
+  },
+  methods: {
+    getUser(msg) {
+      this.user = msg;
+    },
+  },
+};
+</script>
+
+
+
+// 子组件
+<template>
+  <div id="item">
+    <input type="text" v-model="userName" @change="setUser" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: "",
+    };
+  },
+  methods: {
+    setUser() {
+      this.$emit("transfer", this.userName);
+    },
+  },
+};
+</script>
+
+<style>
+</style>

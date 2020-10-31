fe986b9edd4083cc31db4dde7e1b059a3493b364
chenjia-151 <1020420948@qq.com>
2020-10-31 22:04:54 +0800

    ...

--- js/柯里化/proxy.js
+++ js/柯里化/proxy.js
@@ -1,10 +1,10 @@
-let obj = { a: 2 }
+let obj = { a: 1 }
 let proxyObj = new Proxy(obj, {
     get(target, prop) {
         return prop in target ? target[prop] : 0
     },
     set(target, prop) {
-        target[prop] = 888
+        target[prop] = 666
     }
 })
 
@@ -12,5 +12,5 @@ console.log(proxyObj.a);
 console.log(obj.a);
 console.log(proxyObj.b);
 
-proxyObj.a = 666
+proxyObj.a = 888
 console.log(proxyObj.a);

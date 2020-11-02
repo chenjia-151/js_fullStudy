d940fec4551ce121015d086299a882c03db9c9f9
chenjia-151 <1020420948@qq.com>
2020-11-02 14:28:39 +0800

    vue3.0-豆瓣

---
+++ vue3.0/vue-douban/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+export default {
+  name: 'Home',
+  components: {
+    'v-header': Header
+  }
+}
+</script>
